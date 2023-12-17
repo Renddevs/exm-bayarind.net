2205436292542e323abe652c534465f2d1f0f879
Renddevs <renddevs@gmail.com>
2023-12-17 10:38:06 +0700

    fix infrastructure http request

--- Bayarind.Infrastructure.HttpRequest/DependencyInjection.cs
+++ Bayarind.Infrastructure.HttpRequest/DependencyInjection.cs
@@ -1,18 +1,18 @@
-﻿//using Microsoft.Extensions.DependencyInjection;
-//using Microsoft.Extensions.Configuration;
-//using Bayarind.Infrastructure.HttpRequest.Interface;
-//using Bayarind.Infrastructure.HttpRequest.Service;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Bayarind.Infrastructure.HttpRequest.Interface;
+using Bayarind.Infrastructure.HttpRequest.Service;
 
 namespace Homeplate.Infrastructure.HttpRequest
 {
     public static class DependencyInjection
     {
-        //public static IServiceCollection RegisterHttpRequest(this IServiceCollection services, IConfiguration configuration)
-        //{
+        public static IServiceCollection RegisterHttpRequest(this IServiceCollection services, IConfiguration configuration)
+        {
 
-        //    services.AddTransient<IHttpRequest, HttpRequestServices>();
+            services.AddTransient<IHttpRequest, HttpRequestServices>();
 
-        //    return services;
-        //}
+            return services;
+        }
     }
 }
