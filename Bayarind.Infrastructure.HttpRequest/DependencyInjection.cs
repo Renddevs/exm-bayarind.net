d555969c8111a23f087310dc990b48631509e69e
Renddevs <renddevs@gmail.com>
2023-12-17 10:48:24 +0700

    update fix project reference

--- Bayarind.Infrastructure.HttpRequest/DependencyInjection.cs
+++ Bayarind.Infrastructure.HttpRequest/DependencyInjection.cs
@@ -1,18 +1,18 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
-using Bayarind.Infrastructure.HttpRequest.Interface;
-using Bayarind.Infrastructure.HttpRequest.Service;
+//using Bayarind.Infrastructure.HttpRequest.Interface;
+//using Bayarind.Infrastructure.HttpRequest.Service;
 
-namespace Homeplate.Infrastructure.HttpRequest
+namespace Homeplate.Infrastructure.PaymentGateway
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection RegisterHttpRequest(this IServiceCollection services, IConfiguration configuration)
-        {
+        //public static IServiceCollection RegisterHttpRequest(this IServiceCollection services, IConfiguration configuration)
+        //{
 
-            services.AddTransient<IHttpRequest, HttpRequestServices>();
+        //    services.AddTransient<IHttpRequest, HttpRequestServices>();
 
-            return services;
-        }
+        //    return services;
+        //}
     }
 }
