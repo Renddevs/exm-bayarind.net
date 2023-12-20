2f508ad5a8f19f777d4ecb3ba4f7e77f46256060
Renddevs <renddevs@gmail.com>
2023-12-20 19:05:22 +0700

    update model

--- Vleko.SiPeneliti.Data/ApplicationDBContext.cs
+++ Vleko.SiPeneliti.Data/ApplicationDBContext.cs
@@ -2,19 +2,96 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Vleko.Bayarind.Data.Model;
 
 
 namespace Vleko.Bayarind.Data
 {
     public partial class ApplicationDBContext : DbContext
     {
+        public virtual DbSet<TTransaction> t_transaction { get; set; }
+
         public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
         {
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            
+            modelBuilder.Entity<TTransaction>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.ToTable("t_transaction");
+
+                entity.Property(e => e.AuthCode)
+                    .HasMaxLength(350)
+                    .IsUnicode(false)
+                    .HasColumnName("AUTH_CODE");
+
+                entity.Property(e => e.CallbackUrl)
+                    .HasMaxLength(300)
+                    .IsUnicode(false)
+                    .HasColumnName("CALLBACK_URL");
+
+                entity.Property(e => e.ChannelId)
+                    .HasMaxLength(25)
+                    .IsUnicode(false)
+                    .HasColumnName("CHANNEL_ID")
+                    .UseCollation("SQL_Latin1_General_CP1_CI_AS");
+
+                entity.Property(e => e.Currency)
+                    .HasMaxLength(5)
+                    .IsUnicode(false)
+                    .HasColumnName("CURRENCY");
+
+                entity.Property(e => e.CustomerAccount)
+                    .HasMaxLength(250)
+                    .IsUnicode(false)
+                    .HasColumnName("CUSTOMER_ACCOUNT");
+
+                entity.Property(e => e.CustomerName)
+                    .HasMaxLength(150)
+                    .IsUnicode(false)
+                    .HasColumnName("CUSTOMER_NAME");
+
+                entity.Property(e => e.Description)
+                    .HasColumnType("text")
+                    .HasColumnName("DESCRIPTION");
+
+                entity.Property(e => e.FlagType)
+                    .HasMaxLength(10)
+                    .IsUnicode(false)
+                    .HasColumnName("FLAG_TYPE");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.ProcessFds).HasColumnName("PROCESS_FDS");
+
+                entity.Property(e => e.ServiceCode).HasColumnName("SERVICE_CODE");
+
+                entity.Property(e => e.TransactionAmmoun).HasColumnName("TRANSACTION_AMMOUN");
+
+                entity.Property(e => e.TransactionDate)
+                    .HasColumnType("datetime")
+                    .HasColumnName("TRANSACTION_DATE");
+
+                entity.Property(e => e.TransactionExpire)
+                    .HasColumnType("datetime")
+                    .HasColumnName("TRANSACTION_EXPIRE");
+
+                entity.Property(e => e.TransactionMessage)
+                    .HasMaxLength(100)
+                    .IsUnicode(false)
+                    .HasColumnName("TRANSACTION_MESSAGE");
+
+                entity.Property(e => e.TransactionNo).HasColumnName("TRANSACTION_NO");
+
+                entity.Property(e => e.TransactionStatus)
+                    .HasMaxLength(10)
+                    .IsUnicode(false)
+                    .HasColumnName("TRANSACTION_STATUS");
+            });
+
             OnModelCreatingPartial(modelBuilder);
         }
 
---
+++ Vleko.SiPeneliti.Data/Model/TTransaction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Vleko.DAL.Interface;
+
+
+namespace Vleko.Bayarind.Data.Model 
+{
+    public partial class TTransaction : IEntity
+    {
+        public int? Id { get; set; }
+        public string ChannelId { get; set; }
+        public int? ServiceCode { get; set; }
+        public string Currency { get; set; }
+        public int? TransactionNo { get; set; }
+        public int? TransactionAmmoun { get; set; }
+        public DateTime? TransactionDate { get; set; }
+        public DateTime? TransactionExpire { get; set; }
+        public string Description { get; set; }
+        public string CallbackUrl { get; set; }
+        public string CustomerAccount { get; set; }
+        public string CustomerName { get; set; }
+        public string AuthCode { get; set; }
+        public int? ProcessFds { get; set; }
+        public string TransactionStatus { get; set; }
+        public string TransactionMessage { get; set; }
+        public string FlagType { get; set; }
+    }
+}
